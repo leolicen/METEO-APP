eeff6bd426d104e7c3b66995432adf30352a9656
Leonardo Licen <leolicen@yahoo.it>
2023-06-29 17:18:40 +0200

    aggiunta slideshow home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  img: string =  "";
+
+  img1: string = "../../assets/img/Perugia.jpg";
+  img2: string = "../../assets/img/roma.jpg";
 
 
-}
+
+  ngOnInit(): void {
+      this.img = this.img1;
+      setInterval(() => {
+        if(this.img === this.img2){
+          this.img = this.img1
+        } else {
+          this.img = this.img2;
+        }
+      }, 6000)
+    }
+
+  }
+
+
+
+
