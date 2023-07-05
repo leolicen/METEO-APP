abd577ce1b45898b19ba47bd5dbf8f3ad5145944
Leonardo Licen <leolicen@yahoo.it>
2023-07-05 17:21:55 +0200

    immagini home responsive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,22 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit{
 
-  img: string =  "";
+  imgXL: string = "";
+  imgS: string = "";
+  imgXS: string = "";
+
+  pic1Big: string = "../../assets/img/Perugia.jpg";
+  pic2Big: string = "../../assets/img/roma.jpg";
+  pic1Small: string = "../../assets/img/Perugia copia.jpg";
+  pic2Small: string = "../../assets/img/roma copia.jpg";
+  pic1XS: string = "../../assets/img/Perugia copia 2.jpg";
+  pic2XS: string = "../../assets/img/roma copia 2.jpg";
+
+ /*  img: string =  "";
 
   img1: string = "../../assets/img/Perugia.jpg";
-  img2: string = "../../assets/img/roma.jpg";
-
-
+  img2: string = "../../assets/img/roma.jpg"; */
 
   ngOnInit(): void {
-      this.img = this.img1;
+    this.imgXL = this.pic1Big;
+    this.imgS = this.pic1Small;
+    this.imgXS = this.pic1XS;
+    setInterval(() => {
+      if(this.imgXL === this.pic2Big && this.imgS === this.pic2Small && this.imgXS === this.pic2XS){
+        this.imgXL = this.pic1Big;
+        this.imgS = this.pic1Small;
+        this.imgXS = this.pic1XS;
+      } else {
+        this.imgXL = this.pic2Big;
+        this.imgS = this.pic2Small;
+        this.imgXS = this.pic2XS;
+      }
+    }, 5940);
+
+
+     /*  this.img = this.img1;
       setInterval(() => {
         if(this.img === this.img2){
           this.img = this.img1
         } else {
           this.img = this.img2
         }
-      }, 6000)
+      }, 5940) */
     }
 
   }
